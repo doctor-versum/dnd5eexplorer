c4080003f64a23c8ef8d8e7c30ed8faabbc213c6
back_from_black <tobias.hoenisch08@gmail.com>
2025-05-03 22:23:18 +0200

    added icons

--- sw.js
+++ sw.js
@@ -1,9 +1,24 @@
-// sw.js
-self.addEventListener('install', event => {
-    console.log('Service Worker installiert.');
-  });
-  
-  self.addEventListener('fetch', event => {
-    // Optional: Offline-Cache-Logik
-  });
-  
+const cacheName = 'my-pwa-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/main.js',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then((cache) => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
